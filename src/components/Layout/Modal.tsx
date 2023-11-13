a2246593b40a5bd2374578271a0a3e37f85769e2
NeoxAzrot <sami.lafrance@akwai.co>
2023-11-13 15:25:54 +0100

    fix(Modal): add border radius

--- src/components/Layout/Modal.tsx
+++ src/components/Layout/Modal.tsx
@@ -11,7 +11,7 @@ export const Modal: FC<ModalProps> = ({ open, onClose, children }) => {
   return (
     <>
       {open && (
-        <div className="bg-gray-900 w-full max-w-[550px] h-full max-h-[900px] absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 shadow-modal z-10">
+        <div className="rounded-lg bg-gray-900 w-full max-w-[550px] h-full max-h-[900px] absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 shadow-modal z-10">
           <div className="relative w-full h-full">
             <div className="absolute top-3.5 right-3.5">
               <Button
